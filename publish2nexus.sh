8b9b5476e56b82c5b4a543878c25f6f876a99e58
denischerkasov <34245281+denischerkasov@users.noreply.github.com>
2021-05-11 13:29:45 +0300

    Update and rename 444.txt to publish2nexus.sh

---
+++ publish2nexus.sh
@@ -0,0 +1,46 @@
+
+#!/bin/bash -e
+
+cd $WORKSPACE/$BASE_DIR
+version_check=$(cat pom.xml | grep "version" | grep -c "SNAPSHOT")
+version=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+
+if [ $version_check -gt 0 ]; then
+    # Publish gateway-java jar files to nexus
+    echo "Pushing $version.jar to Nexus.."
+    mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+      -DupdateReleaseInfo=true \
+      -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version.jar \
+      -Djavadoc=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version-javadoc.jar \
+      -DpomFile=$WORKSPACE/$BASE_DIR/pom.xml \
+      -DrepositoryId=hyperledger-snapshots \
+      -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
+      -DgroupId=org.hyperledger.fabric-gateway-java \
+      -Dversion=$version \
+      -DartifactId=fabric-gateway-java \
+      -DuniqueVersion=false \
+      -Dpackaging=jar \
+      -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+      echo "========> DONE <======="
+else
+    # Publish gateway-java jar files to nexus
+    echo "Pushing $version-SNAPSHOT.jar to Nexus.."
+    mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+      -DupdateReleaseInfo=true \
+      -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version.jar \
+      -Djavadoc=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version-javadoc.jar \
+      -DpomFile=$WORKSPACE/$BASE_DIR/pom.xml \
+      -DrepositoryId=hyperledger-releases \
+      -Durl=https://nexus.hyperledger.org/content/repositories/releases/ \
+      -DgroupId=org.hyperledger.fabric-gateway-java \
+      -Dversion= $version\
+      -DartifactId=fabric-gateway-java \
+      -DuniqueVersion=false \
+      -Dpackaging=jar \
+      -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+      echo "========> DONE <======="
+fi
